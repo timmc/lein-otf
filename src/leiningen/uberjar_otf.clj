7d1965aa24361eeca9d608fedcdd0b2504ec5202
Marshall T. Vandegrift <llasram@gmail.com>
2012-06-11 11:28:38 -0400

    Use a Java loader stub and support/require lein2.

--- src/leiningen/uberjar_otf.clj
+++ src/leiningen/uberjar_otf.clj
@@ -1,5 +1,11 @@
 (ns leiningen.uberjar-otf
-  (:use [leiningen.uberjar :only (uberjar)]))
+  (:require [clojure.java.io :as io])
+  (:use [leiningen.uberjar :only [uberjar]]))
+
+(def source-path "target/lein-otf")
+(def stub-name "lein_otf/loader/Stub.java")
+(def stub-source (io/resource stub-name))
+(def stub-dest (io/file source-path stub-name))
 
 (defn juggle
   "Put in a loader namespace for :main and put the real main namespace in a
@@ -7,13 +13,19 @@ manifest field. The lein-otf loader should already be present as a dependency."
   [project]
   (if-let [real (:main project)]
     (-> project
-        (assoc-in ,,, [:main] 'lein-otf.loader)
-        (update-in ,,, [:aot]
-                       (fnil #(conj % 'lein-otf.loader) []))
-        (assoc-in ,,, [:manifest "lein-otf-real-main"] (str real)))
+        (dissoc ,,, :main)
+        (update-in ,,, [:java-source-paths] (fnil #(conj % source-path) []))
+        (assoc-in ,,, [:manifest "lein-otf-real-main"] (str real))
+        (assoc-in ,,, [:manifest "Main-Class"] "lein_otf.loader.Stub"))
     project))
 
 (defn uberjar-otf
-  "Plugin entrance point."
+  "Build an uberjar with on-the-fly (OTF) Clojure compilation."
   [project & args]
-  (apply uberjar (juggle project) args))
+  (io/make-parents stub-dest)
+  (when-not (.isFile stub-dest)
+    (with-open [source (io/reader stub-source)]
+      (io/copy source stub-dest)))
+  (let [new-meta (update-in (meta project) [:without-profiles] juggle)
+        project (with-meta (juggle project) new-meta)]
+    (apply uberjar project args)))
