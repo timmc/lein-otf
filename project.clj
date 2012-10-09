4ed070daf7be772cafb5083faa6c08ad7ae20760
Tim McCormack <cortex@brainonfire.net>
2012-10-08 21:29:08 -0400

    Add :license to project.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject org.timmc/lein-otf "1.1.0"
   :url "https://github.com/timmc/lein-otf"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Leiningen plugin for non-AOT uberjars"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :eval-in-leiningen true)
