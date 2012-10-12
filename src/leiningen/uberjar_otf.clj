41922e1d3f2b1baa6cf3bb77b68e8fc040c69e67
Tim McCormack <cortex@brainonfire.net>
2012-10-11 21:34:42 -0400

    Cleanups: Don't refer uberjar; no commas in -> clause; warn on no :main

--- src/leiningen/uberjar_otf.clj
+++ src/leiningen/uberjar_otf.clj
@@ -1,19 +1,18 @@
 (ns leiningen.uberjar-otf
-  (:use [leiningen.uberjar :only (uberjar)]))
+  (:require leiningen.uberjar))
 
 (defn juggle
   "Put in a loader namespace for :main and put the real main namespace in a
 manifest field. The lein-otf loader should already be present as a dependency."
   [project]
-  (if-let [real (:main project)]
+  (let [real (:main project)]
     (-> project
-        (assoc-in ,,, [:main] 'lein-otf.loader)
-        (update-in ,,, [:aot]
-                       (fnil #(conj % 'lein-otf.loader) []))
-        (assoc-in ,,, [:manifest "lein-otf-real-main"] (str real)))
-    project))
+        (assoc-in [:main] 'lein-otf.loader)
+        (update-in [:aot] (fnil #(conj % 'lein-otf.loader) []))
+        (assoc-in [:manifest "lein-otf-real-main"] (str real)))
+    (throw (RuntimeException. "lein-otf expects a :main in project.clj"))))
 
 (defn uberjar-otf
   "Plugin entrance point."
-  [project & args]
-  (apply uberjar (juggle project) args))
+  [project]
+  (leiningen.uberjar/uberjar (juggle project)))
