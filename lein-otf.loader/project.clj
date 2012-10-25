041c2e889b09d02f99a6d6b97155f1cd885a2621
Marshall T. Vandegrift <llasram@gmail.com>
2012-10-25 13:57:53 -0400

    Prevent Java loader stub source from ending up in JAR.

--- lein-otf.loader/project.clj
+++ lein-otf.loader/project.clj
@@ -4,4 +4,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Loader stub for non-AOT Clojure uberjars"
   :java-source-paths ["src/java"]
+  :source-paths ["src/clojure"]
   :dependencies [[org.clojure/clojure "1.4.0"]])
