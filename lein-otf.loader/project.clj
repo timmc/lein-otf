612eb7ac00218ba8f8db32711b89911d35964496
Marshall T. Vandegrift <llasram@gmail.com>
2012-10-25 11:38:34 -0400

    Re-work to use plugin automatic hooks and middleware.

---
+++ lein-otf.loader/project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.llasram/lein-otf.loader "1.0.0"
+  :url "https://github.com/llasram/lein-otf"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Loader stub for non-AOT Clojure uberjars"
+  :java-source-paths ["src/java"]
+  :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/lein_otf/hooks.clj
+++ src/lein_otf/hooks.clj
@@ -1,36 +1,6 @@
 (ns lein-otf.hooks
-  (:require [clojure.java.io :as io]
-            [robert.hooke :as rh]
-            [leiningen.uberjar]))
-
-(def source-path "target/lein-otf")
-(def stub-name "lein_otf/loader/Stub.java")
-(def stub-source (io/resource stub-name))
-(def stub-dest (io/file source-path stub-name))
-
-(defn juggle
-  "Put in a loader namespace for :main and put the real main namespace in a
-manifest field. The lein-otf loader should already be present as a dependency."
-  [main project]
-  (if-let [real (or main (:main project))]
-    (-> project
-        (dissoc ,,, :main)
-        (update-in ,,, [:java-source-paths] (fnil #(conj % source-path) []))
-        (assoc-in ,,, [:manifest "lein-otf-real-main"] (str real))
-        (assoc-in ,,, [:manifest "Main-Class"] "lein_otf.loader.Stub"))
-    project))
-
-(defn hook-uberjar-otf
-  "Build an uberjar with on-the-fly (OTF) Clojure compilation."
-  [uberjar project & args]
-  (io/make-parents stub-dest)
-  (when-not (.isFile stub-dest)
-    (with-open [source (io/reader stub-source)]
-      (io/copy source stub-dest)))
-  (let [[main] args, juggle (partial juggle main)
-        new-meta (update-in (meta project) [:without-profiles] juggle)
-        project (with-meta (juggle project) new-meta)]
-    (apply uberjar project args)))
+  (:require [lein-otf.plugin :as otf]))
 
 (defn activate []
-  (rh/add-hook #'leiningen.uberjar/uberjar hook-uberjar-otf))
+  (println "lein-otf.hooks is deprecated! lein-otf hooking is now automatic.")
+  (otf/hooks))
