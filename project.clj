e917fcbec64ba9c68ccd038468fd15b41ec4f5ee
Marshall T. Vandegrift <llasram@gmail.com>
2012-10-25 11:32:56 -0400

    Begin work on next minor release.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.llasram/lein-otf "2.0.0"
+(defproject org.clojars.llasram/lein-otf "2.1.0-SNAPSHOT"
   :url "https://github.com/llasram/lein-otf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :description "Leiningen plugin for non-AOT uberjars"
+  :description "Leiningen plugin for non-AOT Clojure uberjars"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :eval-in-leiningen true)
