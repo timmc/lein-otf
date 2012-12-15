7124ebe86311d95f91f37080ef6f8ace131fe862
Tim McCormack <cortex@brainonfire.net>
2012-12-15 18:15:08 -0500

    Some fussy cleanup (and doc'ing) on my part.

--- src/lein_otf/plugin.clj
+++ src/lein_otf/plugin.clj
@@ -9,26 +9,28 @@ manifest field. The lein-otf loader should already be present as a dependency."
   [main project]
   (if-let [real (or main (:main project))]
     (-> project
-        (dissoc ,,, :main)
-        (assoc-in ,,, [:manifest "lein-otf-real-main"] (str real))
-        (assoc-in ,,, [:manifest "Main-Class"] "lein_otf.loader.Stub"))
+        (dissoc :main)
+        (assoc-in [:manifest "lein-otf-real-main"] (str real))
+        (assoc-in [:manifest "Main-Class"] "lein_otf.loader.Stub"))
     project))
 
 (defn hook-uberjar-otf
   "Build an uberjar with on-the-fly (OTF) Clojure compilation."
-  [uberjar project & args]
-  (let [[main] args, juggle (partial juggle main)
-        new-meta (update-in (meta project) [:without-profiles] juggle)
-        project (with-meta (juggle project) new-meta)]
+  [uberjar project & [main :as args]]
+  (let [juggle* (partial juggle main)
+        new-meta (update-in (meta project) [:without-profiles] juggle*)
+        project (with-meta (juggle* project) new-meta)]
     (apply uberjar project args)))
 
+;; Called by Leiningen when plugin is loaded.
 (defn hooks
   "Apply hook to modify project map when building uberjar."
   [] (rh/add-hook #'leiningen.uberjar/uberjar hook-uberjar-otf))
 
+;; Used by Leiningen to modify projects
 (defn middleware
   "Middleware to inject lein-otf.loader as a dependency."
   [project]
-  (-> (update-in project [:dependencies] concat
-                 `[[org.timmc/lein-otf.loader "1.0.0"]])
-      (update-in ,,,,,,, [:aliases] assoc "uberjar-otf" "uberjar")))
+  (-> project
+      (update-in [:dependencies] conj ['org.timmc/lein-otf.loader "1.0.0"])
+      (update-in [:aliases] assoc "uberjar-otf" "uberjar")))
